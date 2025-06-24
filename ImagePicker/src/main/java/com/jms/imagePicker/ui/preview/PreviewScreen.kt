bb7784875c88301923507a83ce44cb82e5dd2a10
Minsuk <minsuk.jang@influential.co.kr>
2025-06-24 22:12:33 +0900

    refactoring preview feature

--- ImagePicker/src/main/java/com/jms/imagePicker/ui/preview/PreviewScreen.kt
+++ ImagePicker/src/main/java/com/jms/imagePicker/ui/preview/PreviewScreen.kt
@@ -1,9 +1,11 @@
 package com.jms.imagePicker.ui.preview
 
 import android.net.Uri
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -32,7 +34,11 @@ import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -51,6 +57,26 @@ internal fun PreviewScreen(
     val viewModel: PreviewScreenViewModel = viewModel()
     val uiModel by viewModel.uiModel.collectAsStateWithLifecycle()
 
+    BackHandler {
+        onBack(uiModel)
+    }
+
+    PreviewContent(
+        modifier = modifier,
+        uiModel = uiModel,
+        onBack = onBack,
+        onSelect = viewModel::select
+    )
+}
+
+
+@Composable
+private fun PreviewContent(
+    modifier: Modifier = Modifier,
+    uiModel: PreviewScreenUiModel,
+    onBack: (PreviewScreenUiModel) -> Unit = {},
+    onSelect: () -> Unit = {}
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -89,49 +115,64 @@ internal fun PreviewScreen(
                 modifier = Modifier
                     .align(Alignment.TopEnd)
             ) {
-                TextButton(
+                Box(
                     modifier = Modifier
-                        .size(25.dp)
+                        .size(45.dp)
                         .background(
                             color = when (uiModel.selected) {
-                                true -> Color.White
+                                true -> Color.LightGray
                                 false -> Color.Transparent
                             },
                             shape = CircleShape
                         )
                         .border(
-                            border = BorderStroke(width = 2.dp, color = Color.White),
+                            border = BorderStroke(width = 2.dp, color = Color.LightGray),
                             shape = CircleShape
-                        ),
-                    onClick = {
-                        viewModel.select()
-                    },
-                    contentPadding = PaddingValues(0.dp),
-                    colors = ButtonDefaults.textButtonColors(
-                        contentColor = Color.Transparent
-                    )
-                ) {
-                    if (uiModel.selected)
-                        Text(
-                            text = "${uiModel.order}",
-                            style = TextStyle(
-                                fontWeight = FontWeight.W500,
-                                color = Color.Black
-                            )
                         )
+                        .clickable {
+                            onSelect()
+                        }
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "${uiModel.order}",
+                        style = TextStyle(
+                            fontWeight = FontWeight.W500,
+                            color = Color.Black
+                        )
+                    )
                 }
 
-                Spacer(modifier = Modifier.width(15.dp))
+                Spacer(modifier = Modifier.width(8.dp))
             }
         }
     }
 }
 
-
 @Composable
-@Preview(showBackground = true, uiMode = 100)
-private fun Preview_PreviewScreen() {
-    PreviewScreen(
+@Preview(showBackground = true)
+private fun Preview_PreviewContent(
+    @PreviewParameter(PreviewScreenParameter::class) uiModel: PreviewScreenUiModel
+) {
+    PreviewContent(
+        uiModel = uiModel,
         onBack = {}
     )
+}
+
+
+private class PreviewScreenParameter : PreviewParameterProvider<PreviewScreenUiModel> {
+    override val values: Sequence<PreviewScreenUiModel>
+        get() = sequenceOf(
+            PreviewScreenUiModel(
+                uri = "",
+                selected = true,
+                order = 1
+            ),
+            PreviewScreenUiModel(
+                uri = "",
+                selected = false,
+                order = 1
+            )
+        )
 }
